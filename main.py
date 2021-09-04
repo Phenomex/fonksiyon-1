fe3b6f785c0b7e8ebaf3337ee3ef81081336df86
EnesAlgul <86858992+Phenomex@users.noreply.github.com>
2021-09-04 16:00:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+aydan_dakikaya = 24
+ekleme_notu = "dakikadir"
+
+
+def hesaplama(gunler):
+  print( f"{gunler} Gun {gunler * aydan_dakikaya} {ekleme_notu}")
+
+
+hesaplama(11)
+hesaplama(12)
+hesaplama(13)
+hesaplama(14)
+
+
+
+
+
+
